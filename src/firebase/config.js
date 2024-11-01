8efe990651c5e0ae29f6c2da7ee57a1a56e58c2b
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2024-10-31 22:41:33 -0400

    Firebase Config is more secure

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: "thepeople-bbe44.firebaseapp.com",
-  projectId: "thepeople-bbe44",
-  storageBucket: "thepeople-bbe44.appspot.com",
-  messagingSenderId: "781827147867",
-  appId: "1:781827147867:web:109c62bddb8d051916177e"
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
 };
 
 // Initialize Firebase
@@ -19,4 +19,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-export default app
+export default app;
